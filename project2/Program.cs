8d1d1e0ebca3671210d65f9dee65592aa5dbeabe
Ruslan <ruslan.leonoff2014@yandex.ru>
2023-12-06 14:16:07 +0300

    project 2

---
+++ project2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace project {
+
+    class Program {
+
+        static void Main(){
+             byte [] nums = {1, 5, 6, 7, 2};
+            byte res1 = Summa(nums);
+            Console.WriteLine("Результат: " + res1);
+        }
+
+        public static byte Summa(byte[] digits){
+            byte summ = 0;
+            foreach(byte el in digits)
+                summ += el;
+
+                return summ;
+        }
+
+    }
+
+}
+
